59d6fb6f50b0ab268a5f13098c707460c85bc9fc
Kennedy Julius <kennedyjulius@gmail.com>
2026-01-19 12:44:37 +0300

    google sign in set up

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -7,12 +7,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
-        classpath("com.google.gms:google-services:4.4.2") // <-- Google Services classpath
     }
-}
-
-plugins {
-    id("com.google.gms.google-services") version "4.4.4" apply false
 }
 
 allprojects {
